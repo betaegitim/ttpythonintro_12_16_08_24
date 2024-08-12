9a79f3f161858fbc77e2c07bc20cca58060571f4
betaegitim <info@betaegitim.com>
2024-08-12 10:42:56 +0000

    120824_4

---
+++ Egzersizler/dogacan/dogacan_Egzersiz0203.py
@@ -0,0 +1,4 @@
+"""
+var1 = "TÜRK TELEKOM" 
+ifadesini değişken olarak tanımladıktan sonra 5. ve son indiste yer alan karakterleri ekrana yazdırınız.
+"""
---
+++ Egzersizler/onur/onur_Egzersiz0203.py
@@ -0,0 +1,8 @@
+"""
+var1 = "TÜRK TELEKOM" 
+ifadesini değişken olarak tanımladıktan sonra 5. ve son indiste yer alan karakterleri ekrana yazdırınız.
+"""
+
+var1 = "Turktelekom"
+
+print(var1[2])
