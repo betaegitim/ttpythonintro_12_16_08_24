88f81d2e6fda365ebaea68e9c2bb837f22b62733
betaegitim <info@betaegitim.com>
2024-08-12 11:14:39 +0000

    120824_6

--- Egzersizler/cevaplar/Egzersiz1.py
+++ Egzersizler/cevaplar/Egzersiz1.py
@@ -1,3 +1,6 @@
 """
 kullanıcıdan yaş bilgisini alıp `yas` değişkenine aktaran ve  ekrana Yaşınız:.... formatında çıktı üreten python kodunu yazınız
 """
+
+
+
--- Egzersizler/onur/onur_Egzersiz0203.py
+++ Egzersizler/onur/onur_Egzersiz0203.py
@@ -1,8 +1,16 @@
 """
+1. Soru : 
 var1 = "TÜRK TELEKOM" 
 ifadesini değişken olarak tanımladıktan sonra 5. ve son indiste yer alan karakterleri ekrana yazdırınız.
 """
 
-var1 = "Turktelekom"
+var1 = "TÜRK TELEKOM"
+print(var1[5],var1[-1])
 
-print(var1[2])
+"""
+2. Soru: liste = [2,3,5,7,11,13,17,21]
+a. listenin ilk 3 elemanını ekrana yazdıran kod
+b. listelinin son 3 elemanını ekrana yazdıran kod
+c. listeden 2. indisten sonuna kadar 3 er adımla elemanları yazdıran kod
+d. listenin tersinden yazımını yazdıran kod
+"""
