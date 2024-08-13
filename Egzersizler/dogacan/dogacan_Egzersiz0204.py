7a0731aed1a8e2b0a11b5f904c7fe6a9c6ea437f
betaegitim <info@betaegitim.com>
2024-08-13 11:11:27 +0000

    130824_4

---
+++ Egzersizler/dogacan/dogacan_Egzersiz0204.py
@@ -0,0 +1,38 @@
+"""
+
+1. Aşağıdaki üç farklı sözlüğü tek sözlükte birleştiren python kodunu yazınız
+dict1={1:10, 2:20}
+dict2={3:30, 4:40}
+dict3={5:50,6:60}
+Beklenen Çıktı : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+"""
+
+
+
+
+"""sozluk ={1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+2. Sözlükteki en son elemanı silerek ekrana işlem sonucunu yazdırınız
+Beklenen Çıktı :(6,60)
+"""
+
+
+
+"""dict1={1:10, 2:20}
+3. Yukarıdaki sözlüğe bir eleman ekleyiniz. 
+Beklenen Çıktı :{1:10, 2:20, 3:30}
+"""
+
+
+"""liste=[1,2,3,4,5]
+4. 
+    a.Yukarıdaki listeden faydalanarak bir sözlük oluşturun 
+    b.sözlüğün her alamanının karşılığına değer olarak anahtarda bulunan sayısal değerin 10 katını eşitleyin.
+Beklenen Çıktı :
+a. {1:0,2:0,3:0,4:0,5:0}
+b. {1:10,2:20,3:30,4:40,5:50}
+"""
+
+
+"""5. sozluk={1:10,2:20,3:30,4:40,5:50}
+Sözlük içerisine 6 sayısını anahtar olarak değeri 60 olacak şekilde setdefault fonksiyonunu kullanarak ekleyiniz
+"""
