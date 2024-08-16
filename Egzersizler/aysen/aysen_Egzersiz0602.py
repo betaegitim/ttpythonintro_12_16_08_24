01b9663d9334331d796e40a2ac9230262fb9ad23
İbrahim EDİZ <edizdesign@gmail.com>
2024-08-16 11:07:15 +0000

    160824_3

---
+++ Egzersizler/aysen/aysen_Egzersiz0602.py
@@ -0,0 +1,7 @@
+"""
+Bir okulun öğrenci kayıt sistemini modellemek için Nesneye Yönelik Programlama (OOP) kullanacağız. 
+Her öğrencinin adı, soyadı, numarası, not ortalaması gibi özellikleri olduğunu düşünün. 
+Bu sistemi modellemek için hangi sınıfları oluşturursunuz? 
+Bu sınıfların içinde hangi metotlar (fonksiyonlar) bulunur?
+"""
+
